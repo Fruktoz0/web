ad36c21204882021908b072b7d468bfd9c38a09d
fruktoz <bacsajozesff@gmail.com>
2025-09-23 20:14:38 +0200

    Tisztavaros mobil letöltési link berakva

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -7,13 +7,10 @@ function Navbar() {
   return (
     <header className="w-full bg-[#fffff4]" >
       <div className="container mx-auto px-4 py-4 flex justify-between">
-       
         <Link to="/" className="flex items-center gap-2">
           <img src={Logo} alt="Tiszta Város logó" className="h-10 w-auto" />
           <span className="text-lg font-semibold sm:inline">Tiszta Város</span>
         </Link>
-
-        
         <Link to="/login">
           <Button variant="outline" className="hover:bg-[#37958b] hover:text-[white] border-[#37958b]/50 text-[#37958b] bg-[#fffff4]">BEJELENTKEZÉS</Button>
         </Link>
--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -5,33 +5,45 @@ import { Button } from "@/components/ui/button"
 
 function HeroSection() {
   return (
-   <section 
-   className='w-full py-16' 
-   style={{
-    backgroundImage: `url(${HeroBackground})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center 40%',
-    backgroundRepeat: 'no-repeat',
-   }}>
-    <div className="container mx-auto px-4 flex flex-col items-center justify-center gap-12 ">
-      <div className="text-center md:text-middle max-w-xl space-y-6">
-        <h1 className="text-4xl font-bold">
-          Tiszta Város - A közösség ereje a tisztább városért
-        </h1>
-        <p className="text-lg">
-          Csatlakozz hozzánk, és tegyünk együtt a városunk tisztaságáért! Segíts te is a közösségünknek, hogy egy szebb és tisztább környezetben élhessünk.
-        </p>
-        <p className="text-lg">
-           Jelents, szavazz, kövess - mindenki számára átláthatóan.
-        </p>
+    <section
+      className='w-full py-16'
+      style={{
+        backgroundImage: `url(${HeroBackground})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center 40%',
+        backgroundRepeat: 'no-repeat',
+      }}>
+      <div className="container mx-auto px-4 flex flex-col items-center justify-center gap-12 ">
+        <div className="text-center md:text-middle max-w-xl space-y-6">
+          <h1 className="text-4xl font-bold">
+            Tiszta Város - A közösség ereje a tisztább városért
+          </h1>
+          <p className="text-lg">
+            Csatlakozz hozzánk, és tegyünk együtt a városunk tisztaságáért! Segíts te is a közösségünknek, hogy egy szebb és tisztább környezetben élhessünk.
+          </p>
+          <p className="text-lg">
+            Jelents, szavazz, kövess - mindenki számára átláthatóan.
+          </p>
           <Button className="bg-[#009688] text-white hover:bg-[#00796b]">
             Fedezd fel a funkciókat
           </Button>
+          <div>
+            <a href="https://tisztavaros.hu/app/tisztavaros.apk" download target="_blank"
+              class="inline-flex items-center bg-black text-white px-4 py-2 rounded-lg shadow-md hover:bg-gray-800">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M17.523 9.933l1.54-2.665a.75.75 0 00-1.298-.75l-1.563 2.7a8.236 8.236 0 00-7.402 0L7.236 6.518a.75.75 0 00-1.298.75l1.54 2.665A7.99 7.99 0 004 16a8 8 0 1016 0 7.99 7.99 0 00-2.477-6.067z" />
+              </svg>
+              <div class="flex flex-col leading-tight">
+                <span class="text-xs">Letölthető</span>
+                <span class="text-base font-semibold">Android APK</span>
+              </div>
+            </a>
+          </div>
+        </div>
+
       </div>
 
-    </div>
-    
-   </section>
+    </section>
   )
 }
 
